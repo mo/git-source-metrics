ff78a2b2e33ba34a08ace1de513d08f47866670e
Martin Olsson <martin@minimum.se>
2016-07-16 13:03:15 +0200

    Store url params as object rather than "array with properties"
    And some drop trailing whitespace

--- web/utilities.js
+++ web/utilities.js
@@ -1,16 +1,16 @@
 /* Example URL: http://a.com/hello.html?first=blah&second=yada&second=meh
- * Example URL params: get_url_params() == ["first" : "blah", "second" : ["yada", "meh"]]
+ * Example URL params: get_url_params() == {"first" : "blah", "second" : ["yada", "meh"]}
  */
 function get_url_params()
 {
-	params = []
+	params = {}
 	keyvals = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&')
 	for(var i = 0; i < keyvals.length; ++i)
 	{
 		keyval = keyvals[i].split('=')
 		if (params[keyval[0]] == undefined)
-			params[keyval[0]] = keyval[1] 
-		else if (!params[keyval[0]].push) 
+			params[keyval[0]] = keyval[1]
+		else if (!params[keyval[0]].push)
 			params[keyval[0]] = [params[keyval[0]], keyval[1]]
 		else
 			params[keyval[0]].push(keyval[1])
