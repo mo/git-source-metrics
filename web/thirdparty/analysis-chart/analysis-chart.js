7b88409682fc124173a305dc7ef96935ad3ea3dc
Martin Olsson <martin@minimum.se>
2016-09-01 21:49:45 +0200

    Convert CSS #id selectors to classes in component code and fix selector casing

--- web/thirdparty/analysis-chart/analysis-chart.js
+++ web/thirdparty/analysis-chart/analysis-chart.js
@@ -1,41 +1,46 @@
 class AnalysisChart {
-    constructor(element, data, opts) {
+    constructor(rootElement, data, opts) {
         this.onRangeSelectedCallbacks = [];
 
-        element.innerHTML = `
-            <div id="chart_container">
-                    <div id="y_axis"></div>
-                    <div id="chart"></div>
-                    <div id="x_axis"></div>
-                    <div id="annotation_timeline"></div>
-                    <div id="range_slider"></div>
+        this.rootElement = rootElement;
+        this.rootElement.innerHTML = `
+            <div class="main-column">
+                    <div class="y-axis"></div>
+                    <div class="chart-draw-area"></div>
+                    <div class="x-axis"></div>
+                    <div class="annotation-timeline"></div>
+                    <div class="range-slider"></div>
             </div>
-            <div id="rightColumn">
-                <div id="legend"></div>
-                <div id="optionsPanel">
-                    <div id="legendHelpText">
-                        <span id="legendHelpArrow">&#x2B11;</span> <span>too much clutter? uncheck to temporarily hide metrics</span>
+            <div class="right-column">
+                <div class="legend"></div>
+                <div class="options-panel">
+                    <div class="legend-help-text">
+                        <span class="arrow">&#x2B11;</span> <span>too much clutter? uncheck to temporarily hide metrics</span>
                     </div>
-                    <div id="scalingButtonsContainer">
-                        <input type="radio" name="y-axis-scaling" id="btnFixedZero" />
-                        <label for="btnFixedZero">Fixed Y-axis (fit to data, always include zero)</label>
-
-                        <input type="radio" name="y-axis-scaling" id="btnFixed" />
-                        <label for="btnFixed">Fixed Y-axis (fit to data, with padding)</label>
-
-                        <input type="radio" name="y-axis-scaling" id="btnRescale" />
-                        <label for="btnRescale">Rescale Y-axis to currently visible data</label>
+                    <div class="scaling-buttons-container">
+                        <label>
+                            <input type="radio" name="y-axis-scaling" class="btn-fixed-zero" />
+                            Fixed Y-axis (fit to data, always include zero)
+                        </label>
+                        <label>
+                            <input type="radio" name="y-axis-scaling" class="btn-fixed" />
+                            Fixed Y-axis (fit to data, with padding)
+                        </label>
+                        <label>
+                            <input type="radio" name="y-axis-scaling" class="btn-rescale" />
+                            Rescale Y-axis to currently visible data
+                        </label>
                     </div>
                 </div>
                 <div>
-                    <div id="selInactive">Nothing selected. Drag-select a part of the chart using the mouse to create a selection.</div>
-                    <div id="selActive">
+                    <div class="selection-inactive">Nothing selected. Drag-select a part of the chart using the mouse to create a selection.</div>
+                    <div class="selection-active">
                         <table>
-                        <tr><td>From:</td><td id="selLeftTime"></td></tr>
-                        <tr><td>To:</td><td id="selRightTime"></td></tr>
+                        <tr><td>From:</td><td class="selection-start-time"></td></tr>
+                        <tr><td>To:</td><td class="selection-stop-time"></td></tr>
                         </table>
-                        <div id="selDiffContainer"></div>
-                        <div id="selExtraInfo"></div>
+                        <div class="selection-diff-container"></div>
+                        <div class="selection-extra-info"></div>
                     </div>
                 </div>
 
@@ -46,7 +51,7 @@ class AnalysisChart {
         const allSeriesYSpan = allSeriesYMax - allSeriesYMin;
 
         const graph = this.graph = new Rickshaw.Graph( {
-                element: document.querySelector("#chart"),
+                element: rootElement.querySelector(".chart-draw-area"),
                 width: 1100,
                 height: 500,
                 interpolation: "linear",
@@ -57,42 +62,42 @@ class AnalysisChart {
         graph.render();
 
         const distanceFurthestFromZero = Math.max(Math.abs(allSeriesYMin), Math.abs(allSeriesYMax));
-        document.querySelector('#btnFixedZero').addEventListener('click', (ev) => {
-            params["y-axis-scaling"] = "FixedZero";
+        rootElement.querySelector('.btn-fixed-zero').addEventListener('click', (ev) => {
+            params["y-axis-scaling"] = "fixed-zero";
             reloadWithNewUrlParams(params);
         });
 
-        document.querySelector('#btnFixed').addEventListener('click', (ev) => {
-            params["y-axis-scaling"] = "Fixed";
+        rootElement.querySelector('.btn-fixed').addEventListener('click', (ev) => {
+            params["y-axis-scaling"] = "fixed";
             reloadWithNewUrlParams(params);
         });
 
-        document.querySelector('#btnRescale').addEventListener('click', (ev) => {
-            params["y-axis-scaling"] = "Rescale";
+        rootElement.querySelector('.btn-rescale').addEventListener('click', (ev) => {
+            params["y-axis-scaling"] = "rescale";
             reloadWithNewUrlParams(params);
         });
 
-        const allYAxisScalingMethods = ["FixedZero", "Fixed", "Rescale"];
+        const allYAxisScalingMethods = ["fixed-zero", "fixed", "rescale"];
         if (allYAxisScalingMethods.indexOf(params["y-axis-scaling"]) == -1) {
-            params["y-axis-scaling"] = "FixedZero";
+            params["y-axis-scaling"] = "fixed-zero";
         }
-        document.querySelector('#btn' + params['y-axis-scaling']).checked = true;
+        document.querySelector('.btn-' + params['y-axis-scaling']).checked = true;
         switch (params["y-axis-scaling"]) {
-            case "FixedZero":
+            case "fixed-zero":
                 graph.configure({
                     min: Math.min(allSeriesYMin, 0),
                     max: Math.max(allSeriesYMax + 0.1*distanceFurthestFromZero, 0),
                 });
                 graph.render();
                 break;
-            case "Fixed":
+            case "fixed":
                 graph.configure({
                     min: allSeriesYMin - 0.1*allSeriesYSpan,
                     max: allSeriesYMax + 0.1*allSeriesYSpan,
                 });
                 graph.render();
                 break;
-            case "Rescale":
+            case "rescale":
                 graph.configure({
                     min: "auto",
                     max: undefined,
@@ -102,54 +107,54 @@ class AnalysisChart {
         }
 
         if (allSeries.length > 1) {
-            $('#legendHelpText').show();
+            rootElement.querySelector('.legend-help-text').style.display = "block";
         }
 
         function timestampToDateString(x) {
             return (new Date(x*1000)).toISOString().substring(0, 10)
         }
 
-        var x_axis = new Rickshaw.Graph.Axis.X( {
+        var xAxis = new Rickshaw.Graph.Axis.X({
             graph: graph,
             tickFormat: timestampToDateString,
             orientation: "bottom",
             pixelsPerTick: 120,
-            element: document.getElementById('x_axis'),
-        } );
-        x_axis.render();
+            element: rootElement.querySelector('.x-axis'),
+        });
+        xAxis.render();
 
-        const y_axis = new Rickshaw.Graph.Axis.Y( {
-                graph: graph,
-                orientation: 'left',
-                tickFormat: (v) => v.toLocaleString(),
-                element: document.getElementById('y_axis'),
-        } );
-        y_axis.render();
+        const yAxis = new Rickshaw.Graph.Axis.Y({
+            graph: graph,
+            orientation: 'left',
+            tickFormat: (v) => v.toLocaleString(),
+            element: rootElement.querySelector('.y-axis'),
+        });
+        yAxis.render();
 
-        const legend = new Rickshaw.Graph.Legend( {
-                element: document.querySelector('#legend'),
-                graph: graph
-        } );
+        const legend = new Rickshaw.Graph.Legend({
+            element: rootElement.querySelector('.legend'),
+            graph: graph
+        });
 
         const shelving = new Rickshaw.Graph.Behavior.Series.Toggle({
             graph: graph,
             legend: legend
         });
 
-        const hoverDetail = new Rickshaw.Graph.HoverDetail( {
+        const hoverDetail = new Rickshaw.Graph.HoverDetail({
             graph: graph,
             xFormatter: opts.xFormatter,
             yFormatter: function(y) { return y.toLocaleString() }
-        } );
+        });
 
-        const slider = new Rickshaw.Graph.RangeSlider({
+        const rangeSlider = new Rickshaw.Graph.RangeSlider({
             graph: graph,
-            element: document.querySelector('#range_slider')
+            element: rootElement.querySelector('.range-slider')
         });
 
         const annotator = new Rickshaw.Graph.Annotate({
             graph: graph,
-            element: document.querySelector('#annotation_timeline')
+            element: rootElement.querySelector('.annotation-timeline')
         });
 
         const annotations = all_chart_data[params["chart"]]["chart_annotations"]
@@ -158,10 +163,10 @@ class AnalysisChart {
         }
         annotator.update();
 
-        const chartElement = document.querySelector("#chart");
-        chartElement.style.position = "relative";
-        const chartElementBoundingRect = this.chartElementBoundingRect = chartElement.getBoundingClientRect();
-        const selectionOutline = chartElement.appendChild(document.createElement("div"));
+        const chartDrawArea = rootElement.querySelector(".chart-draw-area");
+        chartDrawArea.style.position = "relative";
+        const drawAreaBoundingRect = this.drawAreaBoundingRect = chartDrawArea.getBoundingClientRect();
+        const selectionOutline = chartDrawArea.appendChild(document.createElement("div"));
         selectionOutline.className += "selection-outline";
         selectionOutline.style.position = "absolute";
         selectionOutline.style.border = "1px dashed black";
@@ -169,23 +174,23 @@ class AnalysisChart {
         let isMouseDown = false;
         let selStartX;
 
-        chartElement.addEventListener("mousedown", (ev) => {
+        chartDrawArea.addEventListener("mousedown", (ev) => {
             selStartX = ev.clientX;
-            selectionOutline.style.left = ev.clientX - chartElementBoundingRect.left;
+            selectionOutline.style.left = ev.clientX - drawAreaBoundingRect.left;
             selectionOutline.style.top = 0;
             selectionOutline.style.width = 0;
-            selectionOutline.style.height = chartElement.clientHeight - 2;
+            selectionOutline.style.height = chartDrawArea.clientHeight - 2;
             isMouseDown = true;
             // Selection should remain hidden until cursor has moved at least 10 px.
             hideSelection();
-            chartElement.querySelector(".detail").style.display = "none";
+            chartDrawArea.querySelector(".detail").style.display = "none";
             ev.preventDefault();
         });
-        chartElement.addEventListener("mousemove", (ev) => {
+        chartDrawArea.addEventListener("mousemove", (ev) => {
             const selEndX = ev.clientX;
             if (isMouseDown) {
                 const selectionWidth = Math.abs(selEndX - selStartX);
-                selectionOutline.style.left = Math.min(selStartX, selEndX) - chartElementBoundingRect.left;
+                selectionOutline.style.left = Math.min(selStartX, selEndX) - drawAreaBoundingRect.left;
                 selectionOutline.style.width = selectionWidth;
 
                 this.updateSelectionInfo(selStartX, selEndX);
@@ -195,30 +200,30 @@ class AnalysisChart {
                 }
             }
         });
-        chartElement.addEventListener("mouseup", (ev) => {
+        chartDrawArea.addEventListener("mouseup", (ev) => {
             if (isMouseDown) {
                 isMouseDown = false;
                 const selEndX = ev.clientX;
                 const selectionWidth = Math.abs(selEndX - selStartX);
-                chartElement.querySelector(".detail").style.display = "block";
+                chartDrawArea.querySelector(".detail").style.display = "block";
                 if (selectionWidth < 10) {
                     // Classify this "drag" as a "click" instead and remove selection
                     hideSelection();
                 }
             }
         });
-        chartElement.addEventListener("mouseleave", (ev) => {
+        chartDrawArea.addEventListener("mouseleave", (ev) => {
             if (isMouseDown) {
                 isMouseDown = false;
             }
         });
-        slider.onSlide(() => {
+        rangeSlider.onSlide(() => {
             hideSelection();
         });
         function hideSelection() {
             selectionOutline.style.display = "none";
-            document.querySelector("#selInactive").style.display = "block";
-            document.querySelector("#selActive").style.display = "none";
+            rootElement.querySelector(".selection-inactive").style.display = "block";
+            rootElement.querySelector(".selection-active").style.display = "none";
         }
         hideSelection();
     }
@@ -227,16 +232,16 @@ class AnalysisChart {
         const selLeft = Math.min(selStartX, selEndX);
         const selRight = Math.max(selStartX, selEndX);
         const from = this.getTimeAtChartXCoord(selLeft);
-        const fromTimestamp = this.graph.x.invert(selLeft - this.chartElementBoundingRect.left);
+        const fromTimestamp = this.graph.x.invert(selLeft - this.drawAreaBoundingRect.left);
         const to = this.getTimeAtChartXCoord(selRight);
-        const toTimestamp = this.graph.x.invert(selRight - this.chartElementBoundingRect.left);
-        document.querySelector("#selInactive").style.display = "none";
-        document.querySelector("#selActive").style.display = "block";
+        const toTimestamp = this.graph.x.invert(selRight - this.drawAreaBoundingRect.left);
+        this.rootElement.querySelector(".selection-inactive").style.display = "none";
+        this.rootElement.querySelector(".selection-active").style.display = "block";
 
-        document.querySelector("#selLeftTime").innerText = from.toISOString().replace("T", " ").slice(0, 16);
-        document.querySelector("#selRightTime").innerText = to.toISOString().replace("T", " ").slice(0, 16);
+        document.querySelector(".selection-start-time").innerText = from.toISOString().replace("T", " ").slice(0, 16);
+        document.querySelector(".selection-stop-time").innerText = to.toISOString().replace("T", " ").slice(0, 16);
 
-        const selDiffContainer = document.querySelector("#selDiffContainer");
+        const selDiffContainer = this.rootElement.querySelector(".selection-diff-container");
         selDiffContainer.innerHTML = '';
         [...allSeries].forEach(series => {
             const [firstDatapointInRange, lastDatapointInRange] = this.getFirstAndLastDatapointInRange(fromTimestamp, toTimestamp, series);
@@ -253,19 +258,19 @@ class AnalysisChart {
                 const diffAbsoluteStr = diffPrefix + diffAbsoluteValue.toLocaleString();
 
                 selDiffContainer.innerHTML += `
-                    <div class="diffWrapper">
+                    <div class="diff-wrapper-outer">
                         <span class="colorbox" style="background: ${series.color}"></span>
-                        <div class="diffWrapperRight">
+                        <div class="diff-wrapper-inner">
                             <div>${series.name}</div>
-                            <div class="diffValue">${diffPercentageStr}% (${diffAbsoluteStr})</div>
-                            <div class="diffDetails">
+                            <div class=".diff-value">${diffPercentageStr}% (${diffAbsoluteStr})</div>
+                            <div class="diff-details">
                                 <div><span>From value: </span><span>${firstDatapointInRange.y}</span></div>
                                 <div><span>To value: </span><span>${lastDatapointInRange.y}</span></div>
                             </div>
                         </div>
                     </div>`;
-                selDiffContainer.querySelectorAll(".diffWrapper").forEach((diffWrapper) => {
-                    const diffDetails = diffWrapper.querySelector(".diffDetails");
+                selDiffContainer.querySelectorAll(".diff-wrapper-outer").forEach((diffWrapper) => {
+                    const diffDetails = diffWrapper.querySelector(".diff-details");
                     diffDetails.style.display = "none";
                     diffWrapper.addEventListener("click", () => {
                         diffDetails.style.display = diffDetails.style.display != "block" ? "block" : "none";
@@ -284,7 +289,7 @@ class AnalysisChart {
     }
 
     getTimeAtChartXCoord(x) {
-        return new Date(1000 * this.graph.x.invert(x - this.chartElementBoundingRect.left));
+        return new Date(1000 * this.graph.x.invert(x - this.drawAreaBoundingRect.left));
     }
 
     getFirstAndLastDatapointInRange(timestampFrom, timestampTo, series) {
